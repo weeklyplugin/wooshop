b9ead7b73b245406228e1ab76a93247ed8eb4a98
Vasim Shaikh <vasims.magneto@gmail.com>
2020-12-10 13:34:28 +0530

    Theme Layout WP Functions

--- header.php
+++ header.php
@@ -1,11 +1,14 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html <?php language_attributes(); ?>>
 <head>
-	<title></title>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2.0">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-	<?php wp_head(); ?>
+<?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
 	<header>
 		<section class="site-logo">Logo</section>
 		<section class="site-menus">
