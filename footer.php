2e7ef3470609b11a77e6ff896123715813b3b26c
Vasim Shaikh <vasims.magneto@gmail.com>
2020-12-10 12:34:28 +0530

    Theme Layout Structure Header & Footer

---
+++ footer.php
@@ -0,0 +1,18 @@
+	<footer>
+		<section class="copy-right">Copyright Content</section>
+		<section class="footer-menus">
+<nav class="footer-nav-items">
+				<a href="">Home</a>
+				<a href="">About</a>
+				<a href="">Products</a>
+				<a href="">Contact</a>
+				
+			</nav>
+		 </section>
+		 
+	</footer>
+
+	<?php wp_footer();?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+	<?php wp_head(); ?>
+</head>
+<body>
+	<header>
+		<section class="site-logo">Logo</section>
+		<section class="site-menus">
+			<nav>
+				<a href="">Home</a>
+				<a href="">About</a>
+				<a href="">Products</a>
+				<a href="">Contact</a>
+				
+			</nav>
+		</section>
+	</header>
